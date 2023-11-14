b2f34a42b31b6ec77cd5cece0beedd602922ae0d
phuoc1238899 <145195528+phuoc1238899@users.noreply.github.com>
2023-11-14 11:49:50 +0700

    N7-07 : Thêm sửa xóa admin

---
+++ modules/user/list.php
@@ -0,0 +1,40 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Danh sách khách hàng</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body table-responsive p-0">
+        <table class="table table-head-fixed text-nowrap">
+            <thead>
+            <tr>
+                <th>Mã khách hàng</th>
+                <th>Ảnh</th>
+                <th>Tên khách hàng</th>
+                <th>Vai trò</th>
+                <th>Hành động</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                </td>
+                <td><img src="" width="50" alt=""></td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <!-- <td><span class="tag tag-success">Hết</span></td> -->
+                <td>
+                    <a href="" class="btn btn-warning">Sửa</a>
+                    <a href="" class="btn btn-danger">Xóa</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="card-body">
+            <p>Đang cập nhật dữ liệu</p>
+        </div>
+    </div>
+    <!-- /.card-body -->
+</div>
+<!-- /.card -->
