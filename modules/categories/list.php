8db717b61bb5806936f9e60d9a964d8ba709199f
phuoc1238899 <145195528+phuoc1238899@users.noreply.github.com>
2023-11-14 11:06:46 +0700

    N7-03 :Quen Mat Khau

---
+++ modules/categories/list.php
@@ -0,0 +1,34 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Danh sách loại</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body table-responsive p-0">
+        <table class="table table-head-fixed text-nowrap">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tên Loại</th>
+                <th>Hành động</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                    <a href="" class="btn btn-warning">Sửa</a>
+                    <a href="" class="btn btn-danger">Xóa</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="card-body">
+            <p>Đang cập nhật dữ liệu</p>
+        </div>
+    </div>
+    <!-- /.card-body -->
+</div>
+<!-- /.card -->
